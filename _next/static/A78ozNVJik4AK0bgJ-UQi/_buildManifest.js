4b23fabb07df484339f6861997551d00f5f8fac3
tzt302 <tzt302@users.noreply.github.com>
2026-08-02 20:15:20 +0800

    Deploy Jinling Poker web edition

---
+++ _next/static/A78ozNVJik4AK0bgJ-UQi/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/game_poker//_next/:path+",
+        "destination": "/game_poker/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
